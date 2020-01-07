1214d89404e42a9ab07f76b6b1257e03dff4ebb2
Automate 6500 <automate6500@gmail.com>
2020-01-07 13:34:53 -0800

    Dockerfile and entry point script

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,6 @@ RUN apk add --no-cache \
         which jq
 
 COPY entrypoint.sh /usr/local/bin/entrypoint.sh
+COPY sample_push_event.json /sample_push_event.json
 
 ENTRYPOINT ["entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,22 @@ jq . < $EVENT_PATH
 if jq '.commits[].message, .head_commit.message' < $EVENT_PATH | grep -i -q "$*";
 then
     # do something
-    echo "Found keyword."
+    VERSION=$(date +%F.%s)
+
+    DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
+    DATA="${DATA} $(printf '"target_commitish":"master",')"
+    DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
+    DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
+    DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
+
+    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
+
+    if [[ "${LOCAL_TEST}" == *"true"* ]];
+    then
+        echo "## [TESTING] Keyword was found but no release was created."
+    else
+        echo $DATA | http POST $URL | jq .
+    fi
 # otherwise
 else
     # exit gracefully
